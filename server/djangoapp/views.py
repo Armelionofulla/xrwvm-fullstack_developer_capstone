2f070c30d9003a0d967ae5da69e9851cb083ba82
Armelionofulla <nofullaarmelio01@gmail.com>
2024-10-21 05:55:00 -0400

    fixing urls and views import

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -15,7 +15,7 @@ from .populate import initiate
 
 from .models import CarMake, CarModel
 
-from .restapis import get_request, analyze_review_sentiments, post_review
+from .restapis import get_request, analyze_review_sentiments, post_review, add_review
 
 
 # Get an instance of a logger
